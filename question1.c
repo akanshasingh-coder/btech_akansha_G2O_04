08e9a051df57ba731f8164c35b19d0a6d106a471
akanshasingh-coder <akanshasingh16806@gmail.com>
2026-02-22 14:06:36 +0530

    Add files via upload

---
+++ question1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a, b, c, d, e, Sum;
+    float Percentage;
+    printf("enter the marks of 5 subjects \n");
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    Sum = a + b + c + d + e;
+    Percentage = Sum / 5;
+    printf("the sum of the marks is %d \n", Sum);
+    printf("the percentage of the marks is %f ", Percentage);
+    return 0;
+}
